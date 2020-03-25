a644539441c7ca9f3096e9673c9b6b656e943ab0
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-24 23:14:05 -0300

    Implementing options to list and delete data from incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,6 +1,12 @@
 const connection = require('../database/connection')
 
 module.exports = {
+    async index (req, res) {
+        const incidents = await connection('incidents').select('*')
+
+        return res.json(incidents)
+    },
+    
     async create(req, res) {
         const { title, description, value } = req.body
         const ong_id = req.headers.authorization
@@ -10,5 +16,23 @@ module.exports = {
         })
 
         return res.json({ id })
+    },
+
+    async delete(req, res) {
+        const { id } = req.params
+        const ong_id = req.headers.authorization
+
+        const incident = await connection('incidents')
+        .where('id', id)
+        .select('ong_id')
+        .first()
+
+        if(incident.ong_id != ong_id) {
+            return Response.status(401).json({ error: 'Operation denied'})
+        }
+        
+        await connection('incidents').where('id', id).delete()
+
+        return res.status(204).send()
     }
 }
