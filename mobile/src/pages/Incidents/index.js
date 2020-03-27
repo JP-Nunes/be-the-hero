6c071420d92a2807a3e4d293419dec545ef392c4
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-26 23:10:22 -0300

    Implementing pagination

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,18 +12,29 @@ import styles from './styles'
 export default function Incidents() {
     const [incidents, setIncidents] = useState([])
     const [total, setTotal] = useState(0)
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+    
     const navigation = useNavigation()
-
 
     function navigateToDetail(incident) {
         navigation.navigate('Detail', { incident })
     }
 
     async function loadIncidents() {
-        const res = await api.get('incidents')
+        if(loading) { return }
 
-        setIncidents(res.data)
+        if(total > 0 && incidents.length == total) { return }
+
+        setLoading(true)
+
+        
+        const res = await api.get('incidents', { params: { page } })
+
+        setIncidents([...incidents, ...res.data])
         setTotal(res.headers['x-total-count'])
+        setPage(page + 1)
+        setLoading(false)
     }
 
     useEffect(() => { loadIncidents() }, [])
@@ -45,6 +56,8 @@ export default function Incidents() {
                 style={styles.incidentList}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
